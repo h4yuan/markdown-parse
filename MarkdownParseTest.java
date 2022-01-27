8dacc59f5b87330fc61bc378144858e9ad00d425
h4yuan <h4yuan@ucsd.edu>
2022-01-26 19:02:55 -0800

    test1

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -2,10 +2,19 @@
 //java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore MarkdownParseTest
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.io.IOException;
 
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void test1() throws IOException{
+        assertEquals(List.of("https://something.com","some-page.html"),MarkdownParse.getLinks(Files.readString(Path.of("test-file.md"))));
     }
 }
